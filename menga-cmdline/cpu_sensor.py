4e4747f74f5bd7f2ee6a7a72d7af5329811cca78
Matthieu Prezut <jesa974@gmail.com>
2022-03-08 14:21:36 +0100

    Create cpu_sensor.py

---
+++ menga-cmdline/cpu_sensor.py
@@ -0,0 +1,12 @@
+from sensor import Sensor
+
+class Cpu_sensor(Sensor):
+
+    def start(self):
+        print("starting")
+    
+    def stop(self):
+        print("starting")
+
+    def get_files(self):
+        print("get files")   
