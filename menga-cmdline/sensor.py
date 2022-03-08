f8dd2e46e722ea443b1098f99275a5d23830d76e
Valentin <valentin.bru@isen.yncrea.fr>
2022-03-08 14:01:15 +0100

    Abstract class in order to manipulate sensor

---
+++ menga-cmdline/sensor.py
@@ -0,0 +1,18 @@
+from abc import ABCMeta, abstractmethod
+
+class Sensor(metaclass=ABCMeta):
+    
+    @staticmethod
+    @abstractmethod
+    def start():
+        """Abstract method to run sensor"""
+
+    @staticmethod
+    @abstractmethod
+    def stop():
+        """Abstract method to run sensor"""
+
+    @staticmethod
+    @abstractmethod
+    def get_files():
+        """Abstract method to run sensor"""   
