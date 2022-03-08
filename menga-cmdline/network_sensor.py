d58af3a7b549e3b007ee90c138e42e0ca64b56f3
Valentin <valentin.bru@isen.yncrea.fr>
2022-03-08 15:27:03 +0100

    Class for network sensor implement sensor

--- menga-cmdline/network_sensor.py
+++ menga-cmdline/network_sensor.py
@@ -1,12 +1,24 @@
 from sensor import Sensor
+import os
+import subprocess
 
 class Network_sensor(Sensor):
 
+    process = ""
+    fpath = str(os.path.abspath('../network-analysis/bcc/tcpconnect.py'))
+    output=""
+
     def start(self):
+        Network_sensor.process = subprocess.Popen(["python3", Network_sensor.fpath, "--output", Network_sensor.output], shell=False)
         print("starting")
     
     def stop(self):
-        print("starting")
+        p = Network_sensor.process
+        p.kill()
+        outs, errs = p.communicate()
+        print('Terminate') 
 
     def get_files(self):
-        print("get files")   
+        resultPath = str(os.path.abspath('../network-analysis/bcc/'+Network_sensor.output))
+        print("get files")
+        return resultPath
